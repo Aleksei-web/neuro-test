9267f173ef28f6751abe4b8adbd5c1c227625af7
amaksimushkin <amaksimushkin@mosline.ru>
2023-05-10 08:36:14 +0300

    first commit

---
+++ src/components/numbers/NumberBig.tsx
@@ -0,0 +1,3 @@
+export const NumberSmall = () => {
+  return <div></div>
+}
---
+++ src/screen/numbers/Numbers.tsx
@@ -0,0 +1,8 @@
+export const Numbers = () => {
+  return <div>
+    <div style={{backgroundColor: 'gray', borderRadius: '50px', height: '100px', width: '100px'}}>
+      <span>1</span>
+    </div>
+    <div>2</div>
+  </div>
+}
